8273e61ea102caa1e1513b2b1745c3d5fe24055b
𝓖∘𝕧𝕠𝕚𝖉𝖆 <zikmuytb3@gmail.com>
2025-07-12 12:02:45 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,30 @@
+// --- SECTION À MODIFIER DIRECTEMENT POUR DÉFINIR LES FICHES ---
+// Chaque objet dans ce tableau représente une fiche d'amitié.
+// MODIFIEZ CE TABLEAU pour ajouter, retirer ou modifier des personnes et leurs points.
+const usersData = [
+    {
+        firstName: 'Sybille',
+        lastName: 'Vignal',
+        password: 'motdepassejean',
+        points: 150
+    },
+    {
+        firstName: 'Gianni',
+        lastName: 'Blaz',
+        password: 'Exp22',
+        points: 250
+    },
+    {
+        firstName: 'Sophie',
+        lastName: 'Martin',
+        password: 'passsophie',
+        points: 80
+    },
+    // Ajoutez d'autres personnes ici :
+    // {
+    //     firstName: 'TonPrénom',
+    //     lastName: 'TonNom',
+    //     password: 'TonMotDePasse',
+    //     points: 100
+    // }
+];
