a453bd7ee410612b43b0b0ae5da8e4e677919e6e
𝓖∘𝕧𝕠𝕚𝖉𝖆 <zikmuytb3@gmail.com>
2025-08-30 23:12:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Rangs selon points
 function getRank(points) {
-    if (points <= 0) return 'Admin';
     if (points === 0) return '❌ Suspendue';
+    if (points <= 0) return 'Admin';
     if (points <= 9) return '🕶️ Connaissance';
     if (points <= 24) return '👋 Camarade';
     if (points <= 49) return '😊 Pote';
