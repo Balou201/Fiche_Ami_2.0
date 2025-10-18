a7174e39dfaab2fa3ef3f894c9f9a0efd0133b0a
𝓖∘𝕧𝕠𝕚𝖉𝖆 <zikmuytb3@gmail.com>
2025-10-18 09:57:07 +0200

    Update data.js

--- data.js
+++ data.js
@@ -91,7 +91,7 @@ const usersData = [
     },
     {
         identifiant: 'Kiwi143',
-        password: 'Gianniestbizzare123',
+        password: 'Error404',
         points: 51,
         birthday: '06-27',
         notes: 'Pour l’instant, tu es très gentil 🙏, on va voir comment ça évolue 🔍',
