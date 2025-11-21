a2d93f502d6354952f38bf8d9fe20e687836c24d
HelloProtect <gianni.blazquez@gmail.com>
2025-11-21 16:32:50 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,8 @@
 const usersData = [
     {
         identifiant: 'mkprod',
-        password: 'Boubourette11@',
+        // Password: 'Boubourette11@' -> Hashed
+        password: 'e5730d1d2797e5564c7406a742cecc74092b7405e608064d4d125439589d892d', 
         points: 0, // 176
         birthday: '12-13',
         notes: 'Elle partage presque les mêmes centres d’intérêt, et elle est gentille et marrante. Un peu en froid en ce moment',
@@ -10,7 +11,8 @@ const usersData = [
     },
     {
         identifiant: 'g.voida', // Compte administrateur
-        password: 'Exp22',
+        // Password: 'Exp22' -> Hashed
+        password: 'c8065b7466c1b3f6f1c7d23f381015f60633b4904f4a38380290539828555894',
         points: -1,
         birthday: '01-01',
         notes: 'Administrateur du site.',
@@ -19,7 +21,8 @@ const usersData = [
     },
     {
         identifiant: 'BunnyFunnyBestship',
-        password: 'G14nn1LEGOAT',
+        // Password: 'G14nn1LEGOAT' -> Hashed
+        password: '7b707470f1a238622c1b181e18d3637651a1a9e52565cc547a46c31043329241',
         points: 88,
         birthday: '03-04',
         notes: '👉 Occupée, Flemme de beaucoup de chose ect...',
@@ -28,7 +31,8 @@ const usersData = [
     },
     {
         identifiant: 'Jo2000',
-        password: 'XXX2025/2026',
+        // Password: 'XXX2025/2026' -> Hashed
+        password: '7b54a883a9366e6b010427847c23a7a93574c885e3b677a29e00949d4791e847',
         points: 83,
         birthday: '04-10',
         notes: '💻 Fan d’informatique',
@@ -37,16 +41,18 @@ const usersData = [
     },
     {
         identifiant: 'louise.94z',
-        password: 'Giannietlouise',
-        points: 0, // 176
-        birthday: '03-13',
-        notes: 'Ami de longue date inactive qui ne repond pas au appel est tres occupé de prend pas le temps de parler envoie pas de message ect... Compliqué',
+        // Password: 'Giannietlouise' -> Hashed
+        password: '01511a3d922f5f14e59040c5f242d7653a067a9e33469c47e81404c000d02b70',
+        points: 0, // 175
+        birthday: '04-12',
+        notes: 'Aime rigoler, et est en terminal, me rappelle des vieux souvenir et se fait beaucoup de souci. Ne me parle plus jamais envoie pas de message ect... Compliqué',
         numberOfCrosses: 0,
         blocage: 0
     },
     {
         identifiant: 'Grizou',
-        password: 'Grizouthegoats',
+        // Password: 'Grizouthegoats' -> Hashed
+        password: '5d098e9185481710927c3f309a633c64c8c707d729486c8d76985f3964d4b17f',
         points: 167,
         birthday: '07-05',
         notes: 'A besoin de me faire un peu plus confiance et moins au gens que tu connais pas et ne pas croire trop les infulenceurs.',
@@ -55,7 +61,8 @@ const usersData = [
     },
     { 
         identifiant: 'Papa',
-        password: '!ticket',
+        // Password: '!ticket' -> Hashed
+        password: '7d562b884c562506b12a02b85e0591f4d9941a547fae51b32d18476d05904d05',
         points: 0, // Inactif - 90
         birthday: '07-07',
         notes: 'Passionné d’informatique, de montage et de jeux vidéo, plutôt sympa.',
@@ -64,7 +71,8 @@ const usersData = [
     },
     {
         identifiant: 'MariDfx',
-        password: '1234',
+        // Password: '1234' -> Hashed
+        password: '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4',
         points: 77,
         birthday: '08-19',
         notes: '🎪 Stalkeuse, fan de cirque, apparemment BE, Bon courage',
@@ -73,7 +81,8 @@ const usersData = [
     },
     {
         identifiant: 'Sim',
-        password: 'simsim36785_',
+        // Password: 'simsim36785_' -> Hashed
+        password: '03c62181b371190d65b399201f81d1134268e3a09725f05786411516e8b4e724',
         points: 87,
         birthday: '12-24',
         notes: 'A surement du Potentiel , et très gentil voir trop parfois, réactif',
@@ -82,10 +91,11 @@ const usersData = [
     },
     {
         identifiant: 'Kiwi143',
-        password: 'BonsoirParis25',
-        points: 71,
-        birthday: '06-27',
-        notes: 'Pour l’instant, tu es très gentil, pas trop trop interactive a besoin que on vienne lui parler pour pouvoir engager une disscusion.',
+        // Password: 'BonsoirParis25' -> Hashed
+        password: '458055c5e87a221b2d35811776ce4e8af78a58e6e584a284e3e33f389a9f24e9',
+        points: 0, // 70
+        birthday: '12-14',
+        notes: 'Sympa, marrante, geek. Se fait énormément de soucis pour rien (même quand je suis là)',
         numberOfCrosses: 0,
         blocage: 0
     },
